11f52c27b2a411476054c5b5cb0a1b886281e097
DanielMarella <marella.daniel96@gmail.com>
2023-07-14 16:07:53 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -9,5 +9,21 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 
 
 <?php
-    include_once __DIR__ . '/classes/Products.php'
+    include_once __DIR__ . '/classes/Products.php';
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <title>ShopOnline</title>
+</head>
+<body>
+    <h1>ciaoo</h1>
+</body>
+</html>
