db5fa57e0a9c3e91d18569ba65605da5daeb232a
DanielMarella <marella.daniel96@gmail.com>
2023-07-15 17:01:02 +0200

    add font awesome, add image and price to card

--- index.php
+++ index.php
@@ -20,16 +20,18 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>ShopOnline</title>
 </head>
 <body>
 
     <div class="container">
         <div class="row">
+            
             <?php foreach ($products as $product) {?>
-            <div class="card col-3 p-0 m-3" >
+            <div class="card col-3 p-0 m-3 w-25" >
                 <div class="card-header">
+                    <img src="<?php echo $product->image ?>"  class="card-img-top  rounded-0" alt="<?php echo $product->title; ?>">
                     <h2>
                         <?php echo $product->title?>
                     </h2>
@@ -43,6 +45,11 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
                     <h5 class="card-subtitle mb-2 text-muted">
                         <p>Animale:
                             <?php echo $product->animals->type; ?>
+                        </p>
+                    </h5>
+                    <h5 class="card-subtitle mb-2 text-muted">
+                        <p>Prezzo:
+                            <?php echo $product->price . "€" ;?> 
                         </p>
                     </h5>
                 </div>
