4ce2465e769193d9b8a4e62d5665d932b7be3462
DanielMarella <marella.daniel96@gmail.com>
2023-07-15 17:00:44 +0200

    add image and price to the construct of the class

--- classes/Products.php
+++ classes/Products.php
@@ -2,16 +2,19 @@
 <?php
 
 class Products{
-
+    public $image;
     public $title;
     public $animals;
     public $categories;
+    public $price;
 
 
-    function __construct(String $title, Animal $animals, Category $category){
+    function __construct(String $image, String $title, Animal $animals, Category $category, Int $price){
+        $this->image = $image;
         $this->title = $title;
         $this->animals = $animals;
         $this->categories = $category;
+        $this->price = $price;
     }
 }
 
